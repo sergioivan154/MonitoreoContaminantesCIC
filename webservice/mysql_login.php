477ecfeb5c3d980a504b22d02c7cd808fd5698c6
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-05-02 12:55:19 -0500

    web service

---
+++ webservice/mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "localhost");// Nombre del host
+define("DATABASE", "airmxgen_meshliu"); // Nombre de la base de datos
+define("USERNAME", "airmxgen_meshliu"); // Nombre del usuario
+define("PASSWORD", "libelium2007"); // Nombre de la constraseña
+?>
